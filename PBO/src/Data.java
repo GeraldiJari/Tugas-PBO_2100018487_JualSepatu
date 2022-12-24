d2d25e022dae68331724333fd95025a0da0ca4ee
GeraldiJari <96601584+GeraldiJari@users.noreply.github.com>
2022-12-24 10:59:01 +0700

    Update Data.java

--- PBO/src/Data.java
+++ PBO/src/Data.java
@@ -1,11 +1,11 @@
-public class Data extends Hitung{
-    String info(String alamat){
-        return "Alamat Toko ";
-    }
+public class Data extends Hitung implements Aplikasi{
+    private String nama;
+    private String alamat;
 
     public Data(int jumlah){
         super(jumlah);
     }
+   
 
     @Override
     int jml_sepatu(int jml) {
@@ -13,6 +13,49 @@ public class Data extends Hitung{
         return (super.getJumlah(jml));
     }
 
+    @Override
+    public void Login() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void out() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setNama(String namaBaru) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setAlamat(String alamatBaru) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getNama() {
+        
+        // TODO Auto-generated method stub
+        return nama;
+    }
+
+    @Override
+    public String getAlamat() {
+        // TODO Auto-generated method stub
+        return alamat;
+    }
+
+    public String toString(){
+      return
+        "Nama       : "+nama+"\n"+
+        "Alamat            : "+alamat+"\n"+
+        "Provinsi   : "+PROVINSI+"\n"; 
+   }
     
     
     
