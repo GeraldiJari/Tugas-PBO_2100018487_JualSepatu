de6d67d80074aca89e8b87655dfc26fde43470a1
GeraldiJari <96601584+GeraldiJari@users.noreply.github.com>
2022-12-24 10:57:46 +0700

    Upd Aplikasi.java

--- PBO/src/Aplikasi.java
+++ PBO/src/Aplikasi.java
@@ -2,4 +2,12 @@ public interface Aplikasi {
     void Login();
     
     void out();
+    static final String PROVINSI = "Jawa Tengah";
+
+    public void setNama(String namaBaru);
+    public void setAlamat(String alamatBaru);
+    public String getNama();
+    public String getAlamat();
 }
+
+
