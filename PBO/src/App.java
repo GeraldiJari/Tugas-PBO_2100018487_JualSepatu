1d002b0b7bc106b57d715accb37f97fb80cafb8e
GeraldiJari <96601584+GeraldiJari@users.noreply.github.com>
2022-12-24 10:58:19 +0700

    Update App.java

--- PBO/src/App.java
+++ PBO/src/App.java
@@ -1,41 +1,52 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class App {
 
-    protected static void batas(){
+    private static void batas(){
         int i=0;
         while ( i <= 10 ){
             System.out.print("===");
             i++;
         }
         System.out.println();
+        //=======================
     }
 
-    private static void cek(){
-        
+    private static void daftar(){
+        System.out.println("Daftar Sepatu : ");
+        String[][] merk = {{"Mike","1",},{"Adinda ","2"},{"Naiki ","3"}};
+        for(int i=0; i < merk.length; i++){
+            System.out.print(merk[i][0]);System.out.print("\t"+merk[i][1]);
+            System.out.println();
+        }
+        //daftar sepatu
     }
 
     public static void main(String[] args) throws Exception {
         //variabel
         int pilih;
-        int pilih_mobil;//ganti jadi sepatu
-        String[][] merk = {{"Sedan","01",},{"BMW ","02"},{"TAXI ","03"}};
+        int jml;
         //inisialisasi objek
-        Pembeli penyewa = new Pembeli();//sewa ganti jadi beli
-        // Data data = new Data();
+        Pembeli beli = new Pembeli();
         Admin admin = new Admin();
+        
+        Info.test();
         Scanner in = new Scanner(System.in);
+        //input nama admin
         System.out.print("Admin : ");
         String name_admin = in.nextLine();
         admin.setNama_admin(name_admin);
+
         do {
             batas();
             System.out.println("Admin : "+admin.getNama_admin());
             System.out.println("1.Input Nama Pembeli");
-            System.out.println("2.Pilih Sepatu");
-            System.out.println("3.Bayar");
-            System.out.println("4.Cetak");
-            System.out.println("5.Keluar");
+            System.out.println("2.Daftar Sepatu");
+            System.out.println("3.Beli Sepatu");
+            System.out.println("4.Keluar");
             System.out.print("Pilih >> ");
             pilih = in.nextInt();
             in.nextLine();
@@ -45,33 +56,40 @@ public class App {
                 case 1:
                     System.out.print("Masukan Nama : ");
                     String nama = in.nextLine();
-                    penyewa.setNama_pembeli(nama);;
+                    beli.setNama_pembeli(nama);
                     System.out.print("Masukan No Telp : ");
                     String no_hp = in.nextLine();
-                    penyewa.setNo_hp(no_hp);
+                    beli.setNo_hp(no_hp);
                     System.out.print("Masukan Alamat : ");
                     String alamat = in.nextLine();
-                    penyewa.setAlamat(alamat);
+                    beli.setAlamat(alamat);
                 break;
+
                 case 2:
-                if(penyewa.getNama_pembeli()==null){
+                if(beli.getNama_pembeli()==null){
                     //kondisi jika inputan nama tidak dimasukan terlebih dahulu
-                    System.out.println("!!! Inputkan Nama Anda Terlebih Dahulu !!!");    
+                    System.out.println("!!! Inputkan Nama Pembeli Terlebih Dahulu Baru Liat !!!");    
                 }
                 else{
-                    
-                    int jml;
-                    System.out.println("Pembeli : "+penyewa.getNama_pembeli());//ganti jadi pembeli
-                    System.out.println("Berapa sepatu yang anda inginkan (Maks 5) ");
+                    daftar();
+                }
+                break;
+
+                case 3:
+                if(beli.getNama_pembeli()==null){
+                    //kondisi jika inputan nama tidak dimasukan terlebih dahulu
+                    System.out.println("!!! Inputkan Nama Pembeli Terlebih Dahulu, Baru Beli !!!");    
+                }
+                else{ 
+                    System.out.println("Pembeli : "+beli.getNama_pembeli());//ganti jadi pembeli
+                    System.out.println("Berapa sepatu yang anda inginkan  ");
                     jml = in.nextInt();
                     Barang brg = new Barang(jml);
                     brg.jml_sepatu(jml);
-                    for(int i=0; i < merk.length; i++){
-                        System.out.print(merk[i][0]);System.out.print("\t"+merk[i][1]);
-                        System.out.println();
-                    }
+                    //tampilkan daftar sepatu
+                    daftar();
                     batas();
-                    System.out.println("PILIH MOBIL");
+                    System.out.println("PILIH SEPATU");
                     pilih = in.nextInt();
                     if(pilih==1){
                         brg.pilih1();
@@ -83,19 +101,22 @@ public class App {
                         brg.pilih3();
                     }
                     brg.jml_sepatu(jml);
+                    try {
+                        System.out.printf("Tunggu sebentar cuyy....\n", LocalTime.now());
+                        Thread.sleep(5 * 1000); 
+                        System.out.printf("Ok bentar lagi....\n", LocalTime.now());
+                        Thread.sleep(5 * 1000); 
+                        System.out.printf("Selesai cuyy\n", LocalTime.now());
+                        Thread.sleep(3 * 1000); 
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+                
                 break;
-                case 3:
-                Barang barang = new Barang(pilih);
-                System.out.println("Penyewa : "+penyewa.getNama_pembeli());
-                System.out.println("PILIH MOBIL");
-                System.out.print(">> ");
-                pilih_mobil = in.nextInt();
-                barang.pilih1();
-                break;
-                case 4:
+                
             }
         }
-        while (pilih <= 4);
+        while (pilih <= 3);
     }
 }
